971b5783caf8b5e5a5073f1e09d4d53e52efe42d
m0humed <qandilmohammed83@gmail.com>
2025-12-11 10:49:13 +0200

    finish

--- src/Components/TempratureCard.js
+++ src/Components/TempratureCard.js
@@ -1,18 +1,16 @@
 import { Box, Button, Container, Divider, Typography } from "@mui/material";
 import CloudIcon from "@mui/icons-material/Cloud";
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { useEffect } from "react";
+
 import moment from "moment";
 import "moment/min/locales";
 import { useTranslation } from "react-i18next";
-import { useStore, useDispatch } from "react-redux";
-import { Get } from "../redux/features/FetchData/FetchSlice";
+import { useSelector, useDispatch } from "react-redux";
+import { FetchWeather } from "../redux/features/FetchData/FetchSlice";
 moment.locale("ar");
 
 export default function TempratureCard() {
-
-
-  const cityData = useStore((state) => state.Weather.data);
+  const { data, isLoading } = useSelector((state) => state.Weather);
   const setCityData = useDispatch();
   const { t, i18n } = useTranslation();
 
@@ -25,193 +23,184 @@ export default function TempratureCard() {
   }
 
   useEffect(() => {
-    // Make a request for a user with a given ID
-    if (cityData.isNotExist) {
-      setCityData(Get());
-    } else {
-
-      setCityData(Get());
-    }
+    setCityData(FetchWeather(t));
   }, [i18n]);
 
   useEffect(() => {
     i18n.changeLanguage("ar");
   }, []);
-
-  setTimeout(() => {
-    return (
-      <Container
-        maxWidth="sm"
-        sx={{
-          color: "white",
-          direction: i18n.language === "ar" ? "rtl" : "ltr",
+  return isLoading? (
+    <img src={process.env.PUBLIC_URL + "/start.png"} alt="loading" style={{width:"100vw" , height:"100vh"}} />
+  ) : (
+    <Container
+      maxWidth="sm"
+      sx={{
+        color: "white",
+        direction: i18n.language === "ar" ? "rtl" : "ltr",
+      }}
+    >
+      {/* Start Card */}
+      <div
+        style={{
+          height: "100vh",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
         }}
       >
-        {/* Start Card */}
+        {/* Content Start */}
         <div
           style={{
-            height: "100vh",
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
+            background: "rgb(28 52 91/36%)",
+            borderRadius: "10px",
+            padding: "10px",
+            minWidth: "50vw",
+            boxShadow: "0px 11px 1px rgpa(0,0,0,0.05)",
           }}
         >
-          {/* Content Start */}
+          {/* start header */}
           <div
             style={{
-              background: "rgb(28 52 91/36%)",
-              borderRadius: "10px",
-              padding: "10px",
-              minWidth: "50vw",
-              boxShadow: "0px 11px 1px rgpa(0,0,0,0.05)",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              marginBottom: "3px",
             }}
           >
-            {/* start header */}
-            <div
-              style={{
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                marginBottom: "3px",
-              }}
-            >
-              <Typography variant="p" sx={{ fontSize: "40px" }}>
-                {t("city")}
-              </Typography>
-              <Divider
-                orientation="vertical"
-                variant="fullWidth"
-                sx={{
-                  bgcolor: "white",
-                  marginInline: "5px",
-                  borderRadius: "1px",
-                }}
-                flexItem
-              />
-              <Typography variant="p" sx={{ fontSize: "30px" }}>
-                {cityData.currentDate}
-              </Typography>
-            </div>
-            {/* end Header  */}
+            <Typography variant="p" sx={{ fontSize: "40px" }}>
+              {t("city")}
+            </Typography>
             <Divider
-              orientation="horizontal"
-              variant="middle"
-              sx={{ color: "white", bgcolor: "white", borderRadius: "1px" }}
+              orientation="vertical"
+              variant="fullWidth"
+              sx={{
+                bgcolor: "white",
+                marginInline: "5px",
+                borderRadius: "1px",
+              }}
               flexItem
             />
+            <Typography variant="p" sx={{ fontSize: "30px" }}>
+              {data.currentDate}
+            </Typography>
+          </div>
+          {/* end Header  */}
+          <Divider
+            orientation="horizontal"
+            variant="middle"
+            sx={{ color: "white", bgcolor: "white", borderRadius: "1px" }}
+            flexItem
+          />
 
-            {/* Start Content */}
+          {/* Start Content */}
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            {/* start Details */}
             <div
               style={{
                 display: "flex",
-                alignItems: "center",
-                justifyContent: "space-between",
+                alignItems: "start",
+                justifyContent: "start",
+                flexDirection: "column",
+                width: "60%",
               }}
             >
-              {/* start Details */}
+              {/* start Tempreture and Icon */}
               <div
                 style={{
                   display: "flex",
-                  alignItems: "start",
-                  justifyContent: "start",
-                  flexDirection: "column",
-                  width: "60%",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  flexDirection: "row",
+                  width: "100%",
                 }}
               >
-                {/* start Tempreture and Icon */}
-                <div
-                  style={{
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center",
-                    flexDirection: "row",
-                    width: "100%",
-                  }}
-                >
-                  <Typography variant="p" sx={{ fontSize: "90px" }}>
-                    {Math.floor(Number(cityData.temp) - 273.15)}
-                  </Typography>
-                  {/* <WbSunnyIcon sx={{ fontSize: "40px" }} /> */}
+                <Typography variant="p" sx={{ fontSize: "90px" }}>
+                  {Math.floor(Number(data.temp) - 273.15)}
+                </Typography>
+                {/* <WbSunnyIcon sx={{ fontSize: "40px" }} /> */}
 
-                  <img
-                    src={`https://openweathermap.org/img/wn/${cityData.imgLink}@2x.png`}
-                    alt="witherState"
-                  />
-                </div>
-                {/* end Tempreture and Icon */}
-                <div
-                  style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "space-evenly",
+                <img
+                  src={`https://openweathermap.org/img/wn/${data.imgLink}@2x.png`}
+                  alt="witherState"
+                />
+              </div>
+              {/* end Tempreture and Icon */}
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "space-evenly",
+                  alignItems: "center",
+                  width: "100%",
+                  minHeight: "10vh",
+                }}
+              >
+                <Typography
+                  variant="p"
+                  fontSize={40}
+                  sx={{ marginBottom: "20px" }}
+                >
+                  {t(data.description)}
+                </Typography>
+                <Box
+                  sx={{
+                    display: "inline-flex",
                     alignItems: "center",
-                    width: "100%",
-                    minHeight: "10vh",
                   }}
                 >
-                  <Typography
-                    variant="p"
-                    fontSize={40}
-                    sx={{ marginBottom: "20px" }}
-                  >
-                    {t(cityData.description)}
+                  <Typography variant="p" fontSize={20}>
+                    {t("minTemp")} : {data.minmax.min}
                   </Typography>
-                  <Box
+                  <Divider
+                    orientation="vertical"
+                    variant="fullWidth"
                     sx={{
-                      display: "inline-flex",
-                      alignItems: "center",
+                      bgcolor: "white",
+                      marginInline: "5px",
+                      borderRadius: "15px",
                     }}
-                  >
-                    <Typography variant="p" fontSize={20}>
-                      {t("minTemp")} : {cityData.minmax.min}
-                    </Typography>
-                    <Divider
-                      orientation="vertical"
-                      variant="fullWidth"
-                      sx={{
-                        bgcolor: "white",
-                        marginInline: "5px",
-                        borderRadius: "15px",
-                      }}
-                      flexItem
-                    />
-                    <Typography variant="p" fontSize={20}>
-                      {t("maxTemp")} : {cityData.minmax.max}
-                    </Typography>
-                  </Box>
-                </div>
+                    flexItem
+                  />
+                  <Typography variant="p" fontSize={20}>
+                    {t("maxTemp")} : {data.minmax.max}
+                  </Typography>
+                </Box>
               </div>
-              {/* end Details */}
-              {/* start show image */}
-              <div style={{ width: "50%", height: "100%" }}>
-                <CloudIcon sx={{ fontSize: "200px" }} />
-              </div>
-              {/* end show image */}
             </div>
-            {/* End Content */}
+            {/* end Details */}
+            {/* start show image */}
+            <div style={{ width: "50%", height: "100%" }}>
+              <CloudIcon sx={{ fontSize: "200px" }} />
+            </div>
+            {/* end show image */}
           </div>
-          <div
-            style={{ display: "flex", justifyContent: "end", width: "100%" }}
-          >
-            <Button
-              variant="text"
-              sx={{
-                color: "white",
-                fontSize: "20px",
-                padding: "5px",
-                // width: "1px !important",
-                //   border: "1px solid",
-              }}
-              onClick={changeLanguageHandler}
-            >
-              {t("language")}
-            </Button>
-          </div>
-          {/* Content End */}
+          {/* End Content */}
         </div>
-        {/* End Card */}
-      </Container>
-    );
-  }, 3000);
+        <div style={{ display: "flex", justifyContent: "end", width: "100%" }}>
+          <Button
+            variant="text"
+            sx={{
+              color: "white",
+              fontSize: "20px",
+              padding: "5px",
+              // width: "1px !important",
+              //   border: "1px solid",
+            }}
+            onClick={changeLanguageHandler}
+          >
+            {t("language")}
+          </Button>
+        </div>
+        {/* Content End */}
+      </div>
+      {/* End Card */}
+    </Container>
+  );
 }
--- src/redux/features/FetchData/FetchSlice.js
+++ src/redux/features/FetchData/FetchSlice.js
@@ -1,8 +1,16 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
-import moment from "moment";
 
-moment.locale("ar");
+function toArabicDateTime(unixTime, offsetInSeconds, t) {
+  const date = new Date((unixTime + offsetInSeconds) * 1000);
+
+  return date.toLocaleString(t("languagecode"), {
+    weekday: "long", // day name
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+}
 // insider functions
 function getTodayMinMaxTemperature(hourlyData) {
   if (!Array.isArray(hourlyData) || hourlyData.length === 0) {
@@ -37,6 +45,28 @@ function getTodayMinMaxTemperature(hourlyData) {
   };
 }
 
+// First, create the thunk
+export const FetchWeather = createAsyncThunk(
+  "weather/GetWeatherData",
+  async (t) => {
+    const response = await axios.get(
+      "https://api.openweathermap.org/data/3.0/onecall?lat=29.0729812&lon=31.0982562&exclude=alerts,daily,minutely&appid=5dc5b2ca4a231daa21b6e58368987ffd"
+    );
+    return {
+      temp: response.data.current.temp,
+      imgLink: response.data.current.weather[0].icon,
+      description: response.data.current.weather[0].description,
+      currentDate: toArabicDateTime(
+        response.data.current.dt,
+        response.data.timezone_offset,
+        t
+      ),
+      minmax: getTodayMinMaxTemperature(response.data.hourly),
+      isNotExist: false,
+    };
+  }
+);
+
 const initialState = {
   data: {
     temp: null,
@@ -48,36 +78,39 @@ const initialState = {
     offset: "",
     isNotExist: true,
   },
+  status: "idle",
+  error: null,
+  isLoading:true
 };
 
 export const weatherData = createSlice({
   name: "weather",
   initialState,
   reducers: {
-    Get: (currentState) => {
-      axios
-        .get(
-          "https://api.openweathermap.org/data/3.0/onecall?lat=29.0729812&lon=31.0982562&exclude=alerts,daily,minutely&appid=5dc5b2ca4a231daa21b6e58368987ffd"
-        )
-        .then((response) => {
-          currentState = {
-            temp: response.data.current.temp,
-            imgLink: response.data.current.weather[0].icon,
-            description: response.data.current.weather[0].description,
-            currentDate: moment(response.data.current.dt).format("YYYY-MM-DD"),
-            minmax: getTodayMinMaxTemperature(response.data.hourly),
-            currentdt: response.data.current.dt,
-            offset: response.data.timezone_offset,
-            isNotExist: false,
-          };
-          console.log(currentState)
-        }).catch(
-            console.log("Feld to fetch data")
-        );
+    Get: (currentState, actions) => {
+      currentState = actions.payload;
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(FetchWeather.pending, (state, action) => {
+        state.status = "pending";
+        state.isLoading = true;
+      })
+      .addCase(FetchWeather.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.isLoading = false;
+        // Add any fetched posts to the array
+        console.log("hgghgkgjgjkkkkkkkkkkkkkkkkk", action.payload);
+        state.data = action.payload;
+      })
+      .addCase(FetchWeather.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message ?? "Unknown Error";
+      });
   },
 });
 
-export const {Get} = weatherData.actions;
+export const { Get } = weatherData.actions;
 
 export default weatherData.reducer;
